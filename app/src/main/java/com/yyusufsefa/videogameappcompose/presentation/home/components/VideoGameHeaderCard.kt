3cdbf52a8a30087e48e7e2230c831359b3a2da7c
yyusufsefa <yalis2@burgan.com.tr>
2024-07-02 12:35:23 +0300

    bug fixes header card

--- app/src/main/java/com/yyusufsefa/videogameappcompose/presentation/home/components/VideoGameHeaderCard.kt
+++ app/src/main/java/com/yyusufsefa/videogameappcompose/presentation/home/components/VideoGameHeaderCard.kt
@@ -5,9 +5,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -45,7 +45,7 @@ fun VideoGameHeaderCard(
         modifier = modifier
             .fillMaxWidth()
             .clip(RoundedCornerShape(32.dp))
-            .fillMaxHeight(0.24f)
+            .height(200.dp)
             .clickable { onClick?.invoke(videoGame.id ?: 0) }
     ) {
         val context = LocalContext.current
